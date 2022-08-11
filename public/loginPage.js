e9cf91f7711458cabda0ef24081ca969dab0bde8
snezhka003 <snezhanka19@mail.ru>
2022-08-11 20:22:46 +0300

    Курсовая - правка сокращения колбека

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,35 +2,37 @@
 
 let userForm = new UserForm();
 
-// let callback = (methodApi, boxMessage) => data => {methodApi(data, response => {
+let callback = (methodApi, boxMessage) => data => {methodApi(data, response => {
+        if(response.success) {
+            location.reload();
+        } else {
+            boxMessage(response.error);
+        }
+    });
+};
+
+userForm.loginFormCallback = callback(ApiConnector.login, (data) => userForm.setLoginErrorMessage(data));
+
+userForm.registerFormCallback = callback(ApiConnector.register, (data) => userForm.setLoginErrorMessage(data));
+
+// НИЖЕ ИЗНАЧАЛЬНЫЙ ВАРИАНТ, ЕЩЕ НЕ СОКРАЩЕННЫЙ (сохранила на всякий случай)
+
+// userForm.loginFormCallback = data => {
+//     ApiConnector.login(data, response => {
 //         if(response.success) {
 //             location.reload();
 //         } else {
-//             boxMessage(response.error);
+//             userForm.setLoginErrorMessage(response.error);
 //         }
 //     });
 // };
 
-// userForm.loginFormCallback = callback(ApiConnector.login, userForm.setLoginErrorMessage);
-
-// userForm.registerFormCallback = callback(ApiConnector.register, userForm.setRegisterErrorMessage);
-
-userForm.loginFormCallback = data => {
-    ApiConnector.login(data, response => {
-        if(response.success) {
-            location.reload();
-        } else {
-            userForm.setLoginErrorMessage(response.error);
-        }
-    });
-};
-
-userForm.registerFormCallback = data => {
-    ApiConnector.register(data, response => {
-        if(response.success) {
-            location.reload();
-        } else {
-            userForm.setRegisterErrorMessage(response.error);
-        }
-    });
-};
+// userForm.registerFormCallback = data => {
+//     ApiConnector.register(data, response => {
+//         if(response.success) {
+//             location.reload();
+//         } else {
+//             userForm.setRegisterErrorMessage(response.error);
+//         }
+//     });
+// };
