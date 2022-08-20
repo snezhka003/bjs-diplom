abbed22021e0af271c4ff559b4d85e522278948f
snezhka003 <snezhanka19@mail.ru>
2022-08-20 11:42:48 +0300

    убрала комментарии

--- public/loginPage.js
+++ public/loginPage.js
@@ -14,25 +14,3 @@ let func = (methodApi, boxMessage) => data => {methodApi(data, response => {
 userForm.loginFormCallback = func(ApiConnector.login, (data) => userForm.setLoginErrorMessage(data));
 
 userForm.registerFormCallback = func(ApiConnector.register, (data) => userForm.setLoginErrorMessage(data));
-
-// НИЖЕ ИЗНАЧАЛЬНЫЙ ВАРИАНТ, ЕЩЕ НЕ СОКРАЩЕННЫЙ (сохранила на всякий случай)
-
-// userForm.loginFormCallback = data => {
-//     ApiConnector.login(data, response => {
-//         if(response.success) {
-//             location.reload();
-//         } else {
-//             userForm.setLoginErrorMessage(response.error);
-//         }
-//     });
-// };
-
-// userForm.registerFormCallback = data => {
-//     ApiConnector.register(data, response => {
-//         if(response.success) {
-//             location.reload();
-//         } else {
-//             userForm.setRegisterErrorMessage(response.error);
-//         }
-//     });
-// };
